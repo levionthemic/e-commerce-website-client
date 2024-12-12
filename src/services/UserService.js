ce062b066a066c18de50505f9285ae6513ecc85a
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-12-12 23:46:02 +0700

    Sửa lại domain server

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosApi = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://e-commerce-website-server-mu.vercel.app",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
     "Content-Type": "application/json",
